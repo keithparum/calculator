9a07c09a38642c60aeb7ceaee2bd5a3b53ff50db
Keith Parum <23403050+keithparum@users.noreply.github.com>
2025-01-19 01:20:02 +0100

    added first version of functions for calculator

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 'use strict'
 
+
+
+function add(firstNum, secondNum) {
+    return firstNum + secondNum
+}
+
+function substract(firstNum, secondNum) {
+    return firstNum - secondNum
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum
+}
+
+function square(firstNum) {
+    return Math.pow(firstNum, 2)
+}
+
+function percentage() {
+    return "not yet emplemented"
+}
+
+function operate(firstNum, secondNum, operator) {}
+
